50189fd17f6f562e76bf55e2bb37b2b3414eb55a
Dhruvgera <dhruvgera61@gmail.com>
2024-04-12 16:47:12 +0530

    MediMate: Create base app
    Signed-off-by: Dhruvgera <dhruvgera61@gmail.com>

---
+++ lib/homepage.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MediMateApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MediMate',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  bool isChatbotTyping = false;
+  final TextEditingController _textController = TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+      isUserMessage: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _getChatbotResponse(text);
+  }
+
+  Future<void> _getChatbotResponse(String query) async {
+    try {
+      setState(() {
+        isChatbotTyping = true;
+      });
+
+      final response = await http.post(
+        Uri.parse('https://livthecode.lakshaygmz.repl.co/query'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'query': query}),
+      );
+
+      if (response.statusCode == 200) {
+        final responseBody = jsonDecode(response.body);
+        final botResponse = responseBody['output'];
+
+        // Split the response text wherever '\n' is present and create a list of lines.
+        final lines = botResponse.split('\n');
+
+        // Concatenate lines into a single message.
+        final concatenatedMessage = lines.join('\n');
+
+        ChatMessage message = ChatMessage(
+          text: concatenatedMessage,
+          isUserMessage: false,
+        );
+
+        setState(() {
+          _messages.insert(0, message);
+          isChatbotTyping = false;
+        });
+      } else {
+        // Handle API error here
+        print('API Error: ${response.reasonPhrase}');
+        setState(() {
+          isChatbotTyping = false; // Set typing status to false on error.
+        });
+      }
+    } catch (error) {
+      // Handle other errors, such as network issues
+      print('Error: $error');
+      setState(() {
+        isChatbotTyping = false; // Set typing status to false on error.
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var assetsImage = new AssetImage(
+        'assets/robot.png'); //<- Creates an object that fetches an image.
+    var image = new Image(image: assetsImage, fit: BoxFit.cover);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MediMate'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Container(
+              child: image,
+              height: 100,
+              width: 100,
+            ),
+          ),
+          Text(
+            'Personalized Health Assistant',
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium
+                ?.copyWith(fontWeight: FontWeight.w400),
+            textAlign: TextAlign.center,
+          ),
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (_, int index) => _messages[index],
+            ),
+          ),
+          Divider(height: 1.0),
+          isChatbotTyping
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(width: 32.0),
+                      CircularProgressIndicator(), // Typing indicator
+                    ],
+                  ),
+                )
+              : Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).cardColor,
+                  ),
+                  child: _buildTextComposer(),
+                ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius:
+            BorderRadius.circular(25.0), // Adjust the border radius as needed
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Describe your health issue',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            color: Color.fromRGBO(252, 124, 184, 1),
+            onPressed: () => _handleSubmitted(_textController.text),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({required this.text, required this.isUserMessage});
+
+  final String text;
+  final bool isUserMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment:
+            isUserMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: <Widget>[
+          isUserMessage ? SizedBox() : CircleAvatar(child: Icon(Icons.person)),
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.7,
+            ),
+            decoration: BoxDecoration(
+              color: isUserMessage
+                  ? Color.fromRGBO(252, 124, 184, 1)
+                  : Colors.grey[300],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            padding: const EdgeInsets.all(8.0),
+            child: Text(text),
+          ),
+          isUserMessage ? CircleAvatar(child: Icon(Icons.person)) : SizedBox(),
+        ],
+      ),
+    );
+  }
+}
